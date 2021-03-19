e9ab4b27e85a4a171002d96fefdc5471c195ed12
IDNatte <idnatte@gmail.com>
2021-03-20 04:46:18 +0800

    add jwt authorization

--- model/__init__.py
+++ model/__init__.py
@@ -15,39 +15,3 @@ class User(db.Model):
   def check_password(self, raw_passwd):
     return check_password_hash(self.password, raw_passwd)
 
-
-
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from sqlalchemy import Column, ForeignKey, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import relationship
-# from sqlalchemy import create_engine
-# import sys
-
-# Base = declarative_base()
-
-# class User(Base):
-#   __tablename__ = 'user'
-
-#   uid = Column(Integer, primary_key=True)
-#   email = Column(String(255), nullable=False)
-#   password = Column(String(255), nullable=False)
-#   username = Column(String(30), nullable=False, unique=True)
-
-#   def set_password(self, raw_passwd):
-#     self.password = generate_password_hash(raw_passwd)
-
-#   def check_password(self, raw_passwd):
-#     return check_password_hash(self.password, raw_passwd)
-
-#   @property
-#   def serialize(self):
-#     return {
-#       'uid': self.uid,
-#       'username': self.username,
-#       'email': self.email
-#     }
-
-# engine = create_engine('sqlite:///db/database.db')
-# Base.metadata.create_all(engine)
-
--- router/api.py
+++ router/api.py
@@ -1,13 +1,33 @@
-from flask import Blueprint, jsonify, request, g, redirect, url_for, abort
-from datetime import datetime
+from flask import Blueprint, jsonify, request, g, redirect, url_for, abort, current_app
+# from helper import authentication
+from functools import wraps
 from model import User
 from config import db
+import sqlalchemy
 import platform
+import datetime
 import flask
 import time
+import jwt
 import os
 
 api_endpoint = Blueprint('api_endpoint', __name__)
+
+def authentication(f):
+  @wraps(f)
+  def decorator(*args, **kwargs):
+    try:
+      token = token = request.headers['Authorization']
+      data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+      current_user = User.query.filter_by(email=data['carrier']['email'], username=data['carrier']['username']).first()
+    except jwt.exceptions.ExpiredSignature as e:
+      abort(401, {'authorizationError': 'token Expired'})
+
+    except KeyError as e:
+      abort(401, {'authorizationError': 'Invalid authorization header'})
+
+    return f(current_user, *args, **kwargs)
+  return decorator
 
 @api_endpoint.before_app_request
 def before_request():
@@ -18,7 +38,7 @@ def before_request():
 @api_endpoint.app_errorhandler(405)
 @api_endpoint.app_errorhandler(404)
 def errorhandler(error):
-  return jsonify({"status": error.code, "message": error.name})
+  return jsonify({"status": error.code, "message": error.description})
 
 @api_endpoint.route('/api', methods=['GET'])
 def server_info():
@@ -27,7 +47,7 @@ def server_info():
     "python_ver": platform.sys.version,
     "build_status": os.environ['FLASK_ENV'],
     "operating_system": platform.system(),
-    "current_timestamp": datetime.now(),
+    "current_timestamp": datetime.datetime.now(),
     "load_time": g.request_time()
   }
 
@@ -36,7 +56,31 @@ def server_info():
 @api_endpoint.route('/api/auth/login', methods=["POST"])
 def authorization():
   if request.method == "POST":
-    return jsonify({"test": "test"})
+    try:
+      email = request.get_json()['email']
+      password = request.get_json()['password']
+      
+      user_data = User.query.filter_by(email=email).one()
+      passwd_authentication = user_data.check_password(request.get_json()['password'])
+
+      if passwd_authentication:
+        token_exp = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
+        token = jwt.encode({
+          "carrier": {
+            "email": user_data.email,
+            "username": user_data.username
+          },
+          'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1), 
+        }, current_app.config['SECRET_KEY'], algorithm='HS256')
+
+        return jsonify({"access_token": token.decode('UTF-8'), "expired": token_exp})
+
+    except KeyError as e:
+      abort(401, {'authorizationError': 'Not sufficient or wrong argument given'})
+
+    except sqlalchemy.exc.NoResultFound as e:
+      abort(401, {'authorizationError': 'User unavailable'})
+
 
 @api_endpoint.route('/api/auth/register', methods=["POST"])
 def register():
@@ -63,10 +107,19 @@ def register():
     return jsonify(send_back)
 
   except KeyError as e:
-    abort(405)
+    abort(405, {'InvalidRequestBodyError': 'Not sufficient or wrong argument given'})
 
+@api_endpoint.route('/user/me', methods=["GET"])
+@authentication
+def get_me(current_user):
+  # print(current_user.u)
+  return jsonify({
+    "username" : current_user.username,
+    "email": current_user.username,
+    "password": "*****"
+  })
 
-  # if (username & password & email):
-  #   return jsonify({"status": "all_fullfiled"})
-
-  
+@api_endpoint.route('/api/resource', methods=["GET"])
+@authentication
+def resource():
+  return jsonify({'secret': 'himichuu'})
