83eaf787cc3a21390175bb1e20648a7561415e62
IDNatte <idnatte@gmail.com>
2021-04-03 19:44:44 +0800

    hooking logger to Gunicorn, change worker to 5

--- deployment/gu_config.py
+++ deployment/gu_config.py
@@ -1,9 +1,12 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 5
 bind = 'unix:sisv.sock'
 umask = 0o007
 reload = True
 
-accesslog = '-'
-errorlog = '-'
+accesslog = '/var/log/sisv/sisv.access.log'
+errorlog = '/var/log/sisv/sisv.error.log'
+capture_output = True
+loglevel = "info"
