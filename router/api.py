d404d4c84da29c8ff31413dfea96ee35e7b5b5f5
IDNatte <idnatte@gmail.com>
2021-05-09 13:33:40 +0800

    change query based on organization name

--- router/api.py
+++ router/api.py
@@ -3570,7 +3570,8 @@ def general_search():
   carrier = []
   query = request.args.get('query')
   org = Organization.objects(org_name__iexact=query)
-  table = DynamicData.objects(table_name__in=org).all().order_by('-created_on')
+  owner = User.objects(org__in=org).all()
+  table = DynamicData.objects(owner__in=owner).all().order_by('-created_on')
   for x in table:
     payload = {
       "table_id": x.public_id,
